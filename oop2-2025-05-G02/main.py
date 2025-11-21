32f7e383b1ff2857ac6a11ef31eda6d830f84e62
nakamoto007 <109nanakaka@gmail.com>
2025-11-21 12:13:23 +0900

    完成？

--- oop2-2025-05-G02/main.py
+++ oop2-2025-05-G02/main.py
@@ -1,25 +1,24 @@
 import sys
 import threading
+
 from PySide6.QtWidgets import QApplication
+
 from src.camera import CameraViewer
-from src.events import setup_capture_event
 from GUI import MainWindow
 
-if __name__ == "__main__":
+
+def main() -> None:
     camera = CameraViewer()
     threading.Thread(target=camera.run, daemon=True).start()
 
     app = QApplication(sys.argv)
     win = MainWindow(camera)
     win.show()
-    sys.exit(app.exec())
+    exit_code = app.exec()
+
+    camera.stop()
+    sys.exit(exit_code)
 
 
 if __name__ == "__main__":
-    camera = CameraViewer()
-    threading.Thread(target=camera.run, daemon=True).start()
-
-    app = QApplication(sys.argv)
-    win = MainWindow(camera)
-    win.show()
-    sys.exit(app.exec())
+    main()
